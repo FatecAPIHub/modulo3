629e0fad60b11a7f0f0c030d9c731182ea8ff643
bportugal01 <bruno2004sa@gmail.com>
2025-12-02 18:59:14 -0300

    feat(set up environment): docker/docker compose, github hooks, spring framework and dependency

---
+++ src/test/java/br/com/fatec/modulo3/kafka_consumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo3.kafka_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
