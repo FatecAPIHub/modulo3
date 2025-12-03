485d71e84a219e3196b195c9cec825f1da3b2cf2
bportugal01 <bruno2004sa@gmail.com>
2025-12-02 22:10:07 -0300

    feat(Docker compose): Kafka and Zookeeper image configuration Kafka Consumer

--- src/main/java/br/com/fatec/modulo3/kafka_consumer/KafkaConsumerApplication.java
+++ src/main/java/br/com/fatec/modulo3/kafka_consumer/KafkaConsumerApplication.java
@@ -2,6 +2,8 @@ package br.com.fatec.modulo3.kafka_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class KafkaConsumerApplication {
@@ -10,4 +12,9 @@ public class KafkaConsumerApplication {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 
+}
+
+@Component
+class KafkaMessageConsumer {
+    @KafkaListener
 }
